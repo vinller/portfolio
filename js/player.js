d6f5fbe9f4fed089b7f853086d95a426a9f2b8d3
vinller <vincentmiller1224@gmail.com>
2022-10-31 00:44:33 -0400

    tracks

--- js/player.js
+++ js/player.js
@@ -9,80 +9,80 @@ new Vue({
             currentTime: null,
             isTimerPlaying: false,
             tracks: [{
-                    name: "All Night",
-                    artist: "COTIS",
+                    artist: "All Night",
+                    name: "COTIS",
                     cover: "https://cdn.vinnyy.me/songs/artwork/ab67616d0000b2738ceee6f76614b2f4de0a7438.png",
                     source: "https://cdn.vinnyy.me/songs/audio/allnight-cotis.mp3",
                     url: "https://open.spotify.com/track/2Ur81w4umgEMiFDWggKBOG",
                     favorited: false
                 },
                 {
-                    name: "Die For Me (feat. Future & Halsey)",
-                    artist: "Post Malone, Halsey, Future",
+                    artist: "Die For Me (feat. Future & Halsey)",
+                    name: "Post Malone, Halsey, Future",
                     cover: "https://cdn.vinnyy.me/songs/artwork/ab67616d0000b2739478c87599550dd73bfa7e02.png",
                     source: "https://cdn.vinnyy.me/songs/audio/dieforme-postmalone.mp3",
                     url: "https://open.spotify.com/track/2C6WXnmZ66tHhHlnvwePiK",
                     favorited: false
                 },
                 {
-                    name: "Glimpse Of Us",
-                    artist: "Joji",
+                    artist: "Glimpse Of Us",
+                    name: "Joji",
                     cover: "https://cdn.vinnyy.me/songs/artwork/ab67616d00001e0208596cc28b9f5b00bfe08ae7.png",
                     source: "https://cdn.vinnyy.me/songs/audio/glimpseofus-joji.mp3",
                     url: "https://open.spotify.com/track/6xGruZOHLs39ZbVccQTuPZ",
                     favorited: false
                 },
                 {
-                    name: "Lost in the Fire (feat. The Weeknd)",
-                    artist: "Gesaffelstein, The Weeknd",
+                    artist: "Lost in the Fire (feat. The Weeknd)",
+                    name: "Gesaffelstein, The Weeknd",
                     cover: "https://cdn.vinnyy.me/songs/artwork/ab67616d00001e02011a2f51fe757fafe13a0c2e.png",
                     source: "https://cdn.vinnyy.me/songs/audio/lostinfire.mp3",
                     url: "https://open.spotify.com/track/2vXKRlJBXyOcvZYTdNeckS",
                     favorited: false
                 },
                 {
-                    name: "Enough",
-                    artist: "charlieonnafriday",
+                    artist: "Enough",
+                    name: "charlieonnafriday",
                     cover: "https://cdn.vinnyy.me/songs/artwork/ab67616d00001e02d3879d79b66e17a91c59aba8.png",
                     source: "https://cdn.vinnyy.me/songs/audio/enough-charlie.mp3",
                     url: "https://open.spotify.com/track/4vuVzf7BIZhDAFm3TB0Tzy",
                     favorited: false
                 },
                 {
-                    name: "Hit Different",
-                    artist: "SZA, The Neptunes, Pharrell Williams, Ty Dolla $ign",
+                    artist: "Hit Different",
+                    name: "SZA, The Neptunes, Pharrell Williams, Ty Dolla $ign",
                     cover: "https://cdn.vinnyy.me/songs/artwork/ab67616d00001e02a135dd969dce9f38ed32ef98.png",
                     source: "https://cdn.vinnyy.me/songs/audio/hitdifferent-sza.mp3",
                     url: "https://open.spotify.com/track/7Bar1kLTmsRmH6FCKKMEyU",
                     favorited: false
                 },
                 {
-                    name: "do u even miss me at all?",
-                    artist: "gianni & kyle",
+                    artist: "do u even miss me at all?",
+                    name: "gianni & kyle",
                     cover: "https://cdn.vinnyy.me/songs/artwork/ab67616d00001e02f79ce85d7072f57f9cbb6013.png",
                     source: "https://cdn.vinnyy.me/songs/audio/doyouevenmissmeatall.mp3",
                     url: "https://open.spotify.com/track/1AHf5FSofKcUw8tyKkccKF",
                     favorited: false
                 },
                 {
-                    name: "Drinking Problem (feat. 27CLUB)",
-                    artist: "Arizona Zervas, 27CLUB",
+                    artist: "Drinking Problem (feat. 27CLUB)",
+                    name: "Arizona Zervas, 27CLUB",
                     cover: "https://cdn.vinnyy.me/songs/artwork/ab67616d00001e02c1b10601d816a5de948db26f.png",
                     source: "https://cdn.vinnyy.me/songs/audio/drinkingproblems.mp3",
                     url: "https://open.spotify.com/track/7piU1yHaKMD9PaOPvEymOr",
                     favorited: false
                 },
                 {
-                    name: "Better Than the Other Guy",
-                    artist: "Henrie",
+                    artist: "Better Than the Other Guy",
+                    name: "Henrie",
                     cover: "https://cdn.vinnyy.me/songs/artwork/ab67616d00001e02bcb38e774ff4f9474313dc26.png",
                     source: "https://cdn.vinnyy.me/songs/audio/betterthattheotherguy.mp3",
                     url: "https://open.spotify.com/track/2FlQePhuKXvPTpIm967Gda",
                     favorited: false
                 },
                 {
-                    name: "Focus (feat. 21 Savage)",
-                    artist: "Bazzi, 21 Savage",
+                    artist: "Focus (feat. 21 Savage)",
+                    name: "Bazzi, 21 Savage",
                     cover: "https://cdn.vinnyy.me/songs/artwork/ab67616d00001e023a376bd9b9b1f4b2686807db.png",
                     source: "https://cdn.vinnyy.me/songs/audio/focus-bazzi.mp3",
                     url: "https://open.spotify.com/track/2FqqVHvC4eKrsk97vpMRid",
